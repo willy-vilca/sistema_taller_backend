a3d3a7b8ed339ef852800f8abb2c0bf37e4f0c7f
Willy Vilca <willyvilca06@gmail.com>
2026-07-31 15:33:59 -0500

    fix despliegue

--- src/test/java/com/tallermecanico/api/TallerApiApplicationTests.java
+++ src/test/java/com/tallermecanico/api/TallerApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.tallermecanico.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TallerApiApplicationTests {
 
 	@Test
